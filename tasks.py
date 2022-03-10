90aa43b45e1f055602b3038b7de22d96802d04ee
Edvard Majakari <edvard@majakari.net>
2022-03-10 17:45:50 +0200

    Add publish task

--- tasks.py
+++ tasks.py
@@ -5,3 +5,9 @@ from invoke import task
 def test_cov(ctx):
     """Run test coverage"""
     ctx.run("pytest --durations 3 -vv --cov-branch --cov devseed --cov-report term")
+
+
+@task
+def publish(ctx):
+    """Publish to PyPi"""
+    ctx.run("pytest && poetry build && poetry publish")
